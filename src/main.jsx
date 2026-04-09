e758f44559f2c2ef2d47392e7ac2ecc6edcc052b
atahanfurkan <atahanfrkn@gmail.com>
2026-04-09 13:25:33 +0300

    google analytics eklendi

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,26 @@
-import { StrictMode } from 'react'
+import { StrictMode, useEffect } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { BrowserRouter } from 'react-router-dom'
+import ReactGA from "react-ga4"
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+// Google Analytics'i senin kimliğinle başlatıyoruz
+ReactGA.initialize("G-10SS18K0BW");
+
+const RootComponent = () => {
+  useEffect(() => {
+    // Sayfa ilk yüklendiğinde ve değiştikçe Google'a haber ver
+    ReactGA.send({ hitType: "pageview", page: window.location.pathname });
+  }, []);
+
+  return (
+    <StrictMode>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </StrictMode>
+  );
+};
+
+createRoot(document.getElementById('root')).render(<RootComponent />);
